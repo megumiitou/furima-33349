a4e6d7f9306f34e68530e0dd1f34ed577c5a2e6e
megumiitou <78079814+megumiitou@users.noreply.github.com>
2021-04-14 17:39:14 +0900

    バリデーションとテストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, presence: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX}
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-    validates :last_name, presence: true
-    validates :first_name, presence: true
+    validates :last_name
+    validates :first_name
   end
 
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
-    validates :last_name_kana, presence: true
-    validates :first_name_kana, presence: true
+    validates :last_name_kana
+    validates :first_name_kana
   end
 
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+   end
 
 end
