f09d3d029f8d16410abbf2528110780d965c043b
megumiitou <78079814+megumiitou@users.noreply.github.com>
2021-04-19 15:34:40 +0900

    ActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,18 +2,26 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :goods_category
+  belongs_to :goods_condition
+  belongs_to :shipping_charges
+  belongs_to :shipping_area
+  belongs_to :shipping_days
 
   with_options presence: true do
     validates :image
     validates :title
     validates :text
-    validates :goods_category_id
-    validates :goods_condition_id
-    validates :shipping_charges_id
-    validates :shipping_area_id
-    validates :shipping_days_id
     validates :price
     validates :user
+    with_options numericality: { other_than: 1 } do
+      validates :goods_category_id
+      validates :goods_condition_id
+      validates :shipping_charges_id
+      validates :shipping_area_id
+      validates :shipping_days_id
+    end
   end
 
 end
