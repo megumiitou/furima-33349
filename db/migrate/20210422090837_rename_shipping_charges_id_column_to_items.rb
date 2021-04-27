dccd494e72942df76787281b6a8d4008934f7d72
megumiitou <78079814+megumiitou@users.noreply.github.com>
2021-04-27 09:44:17 +0900

    商品の一覧表示機能の実装

--- db/migrate/20210422090837_rename_shipping_charges_id_column_to_items.rb
+++
@@ -1,6 +0,0 @@
-class RenameShippingChargesIdColumnToItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :shipping_charges_id, :shipping_charge_id
-    rename_column :items, :shipping_days_id, :shipping_day_id
-  end
-end
