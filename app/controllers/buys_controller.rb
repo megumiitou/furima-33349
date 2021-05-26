4a12b4dc6bd098decd359a5e126166b7f491b734
megumiitou <78079814+megumiitou@users.noreply.github.com>
2021-05-26 17:43:45 +0900

    before_actionの設定

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,15 +1,16 @@
 class BuysController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, except: [:index]
+  before_action :buy_user
 
   def index
   end
   
   def new
-    @item = Item.find(params[:item_id])
     @buy_form = BuyForm.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buy_form = BuyForm.new(buy_params)
     if @buy_form.valid?
       pay_item
@@ -27,6 +28,10 @@ class BuysController < ApplicationController
     params.require(:buy_form).permit(:postal_code, :shipping_area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def set_buy
+    @item = Item.find(params[:item_id])
+  end
+
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
@@ -34,6 +39,13 @@ class BuysController < ApplicationController
       card: buy_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def buy_user
+    @item = Item.find(params[:item_id])
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
 end
