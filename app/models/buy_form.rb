15e818c96be93cbb4e63a1769c26e120a8279da5
megumiitou <78079814+megumiitou@users.noreply.github.com>
2021-05-27 11:53:40 +0900

    レビューによる修正

--- app/models/buy_form.rb
+++ app/models/buy_form.rb
@@ -5,6 +5,8 @@ class BuyForm
 
   with_options presence: true do
     validates :token
+    validates :user_id
+    validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :city
     validates :address
--- spec/factories/buy_form.rb
+++ spec/factories/buy_form.rb
@@ -3,8 +3,9 @@ FactoryBot.define do
     token                     {"tok_abcdefghijk00000000000000000"}
     postal_code               {'111-1111'}
     shipping_area_id          {2}
-    city                      {Faker::String.random(length: 1..10)}
-    address                   {Faker::String.random(length: 1..20)}
+    city                      {"sample"}
+    address                   {"11"}
+    building_name             {"building"}
     phone_number              {11111111111}
 
     association :user
