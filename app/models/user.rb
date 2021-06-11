a9dae5b427f955ef5d0498eeb121d473a6aa47ed
megumiitou <78079814+megumiitou@users.noreply.github.com>
2021-06-11 20:56:24 +0900

    sns認証の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,24 @@
 class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   has_many :items
   has_many :buys
+  has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, presence: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX}
